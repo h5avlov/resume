3e347328f16ff87017fd91825bebbf0177f56747
h5avlov <55092488+h5avlov@users.noreply.github.com>
2019-10-02 09:52:30 +0300

    Add files via upload

--- plugins/percentage-wheel/percentage-wheel.js
+++ plugins/percentage-wheel/percentage-wheel.js
@@ -8,35 +8,119 @@
 			// of two adjacent fragments
 			const DEFAULT_WRAPPER_CLASS = "wrapper", 
 				DEFAULT_FRAGMENT_CLASS = "fragment", 
-				DEFAULT_FRAGMENT_COUNT = 40, 
-				WRAPPER_CLASS = settings.wrapperClassName || DEFAULT_WRAPPER_CLASS, 
-				FRAGMENT_CLASS = settings.fragmentClassName || DEFAULT_FRAGMENT_CLASS,
-				FRAGMENT_COUNT = settings.fragmentCount || DEFAULT_FRAGMENT_COUNT, 
-				ROTATION_STEP = 360 / FRAGMENT_COUNT,  
-				PERCENTAGE_STEP = 100 / FRAGMENT_COUNT;
+				DEFAULT_EXCLUDED_FRAGMENT_CLASS = "fragment-excluded", 
+				DEFAULT_FRAGMENT_COUNT = 40,  
 				
-			let wrapper = $( `<div class= ${WRAPPER_CLASS} />` ), 
+				// The name of the CSS variable to hold the rotation of each fragment
+				ROTATION_PROPERTY_NAME = "--fragment-rotation", 
+				
+				DEFAULT_ANIMATION_START_DELAY = 0, 
+				DEFAULT_FRAGMENT_ANIMATION_DELAY_SHIFT = 0, 
+				DEFAULT_FRAGMENT_ANIMATION_DURATION = 0.025;
+				
+			const wrapperClass = settings.wrapperClassName || DEFAULT_WRAPPER_CLASS, 
+				wrapper = $( `<div class= ${wrapperClass} />` ), 
+				fragmentClass = settings.fragmentClassName || DEFAULT_FRAGMENT_CLASS,
+				excludedFragmentClass = settings.excludedFragmentClass || DEFAULT_EXCLUDED_FRAGMENT_CLASS, 
+				fragmentCount = settings.fragmentCount || DEFAULT_FRAGMENT_COUNT, 
+				
+				rotationStep = 360 / fragmentCount,  
+				percentageStep = 100 / fragmentCount, 
+				
 				level = settings.level, 
-				exclusionCount = ( 100 - level ) / PERCENTAGE_STEP, 
-				wrapperRotation = exclusionCount / 2 * ROTATION_STEP; 
+				exclusionCount = ( 100 - level ) / percentageStep, 
+				startRotation = -exclusionCount / 2 * rotationStep, 
+				wrapperRotation = exclusionCount / 2 * rotationStep, 
+				animationParameters = getAnimationParameters( ".graphic-representation" ), 
+				animationStartDelay = animationParameters.animationStartDelay || DEFAULT_ANIMATION_START_DELAY, 
+				fragmentAnimationDelayShift = animationParameters.fragmentAnimationDelayShift || DEFAULT_FRAGMENT_ANIMATION_DELAY_SHIFT, 
+				fragmentAnimationDuration = animationParameters.fragmentAnimationDuration || DEFAULT_FRAGMENT_ANIMATION_DURATION;
 				
-			// Rotates the wheel to keep its position symmetrical around the x-axis
-			let symmetricalCorrection = 0;
-			if ( settings.symmetrical ) {
-				symmetricalCorrection = exclusionCount / 2;
+			/* // Initial fragment delay
+			let fragmentDelay = animationStartDelay;  */
+				
+			const fragments = createFragments( fragmentCount, fragmentClass ); 
+			setFragmentRotations( fragments, startRotation, rotationStep, ROTATION_PROPERTY_NAME ); 
+			markExcludedFragments( fragments, exclusionCount, excludedFragmentClass );
+			
+			// getMaxExcludedDelay( fragments, exclusionCount );
+			
+			// Setting animation delays only in case fragments would be animated separately
+			if ( settings.isAnimated ) {
+				setAnimationDelays( fragments, animationStartDelay, fragmentAnimationDuration, fragmentAnimationDelayShift );
 			}
 			
-			// Creates the next fragment 
-			// and rotates it, according to its position
-			for ( let i = 0; i < FRAGMENT_COUNT - exclusionCount; i++ ) {
-				let fragment = $( `<div class= ${FRAGMENT_CLASS} />` ), 
-					rotation = ROTATION_STEP * ( i + symmetricalCorrection );
-				fragment.css( { transform: `rotateZ( ${rotation}deg )` } );
-				wrapper.append( fragment );
-			}
-			
+			// Fragments are appended to a designated wrapper element
+			// and the wrapper to the document after that
+			wrapper.append( fragments );
 			$( this ).append( wrapper );
+			
+			setMaxExcludedDelay( wrapper, fragments, exclusionCount ); 
+			
 		} );
+		
+		
+		// Function declarations
+		
+		
+		function setMaxExcludedDelay( propertyHolder, fragments, exclusionCount ) {
+			const value = getPropertyValue( $( fragments[ exclusionCount - 1 ] ), "--fragment-animation-delay" ); 
+			setPropertyValue( propertyHolder, "--max-excluded-delay", value + "s" ); 
+		}
+		
+		// An animation delay is calculated and a CSS variable to store it, assigned to each fragment
+		function setAnimationDelays( fragments, startDelay, duration, delayShift ) {
+			let fragmentDelay = startDelay; 
+			for ( let i = 0; i < fragments.length; i++ ) {
+				setPropertyValue( fragments[ i ], "--fragment-animation-delay", fragmentDelay + "s" );
+				fragmentDelay += duration + delayShift; 
+			}
+		}
+		
+		// A CSS class is given to the excluded fragments for applying different style to them in CSS
+		function markExcludedFragments( fragments, excludedFragmentCount, excludedFragmentClass ) {
+			for ( let i = 0; i < excludedFragmentCount; i++ ) {
+				fragments[ i ].addClass( excludedFragmentClass );
+			}
+		}
+		
+		// Sets a variable in CSS, holding the rotation for each fragment
+		function setFragmentRotations( fragments, startRotation, rotationStep, rotationPropertyName ) {
+			for ( let i = 0; i < fragments.length; i++ ) {
+				const rotation = startRotation + i * rotationStep;
+				setPropertyValue( fragments[ i ], rotationPropertyName, rotation + "deg" ); 
+			}
+		}
+		
+		// Creates a list of the specified number of fragments
+		function createFragments( fragmentCount, fragmentClass ) {
+			let fragments = [];
+			while ( fragmentCount-- > 0 ) {
+				const fragment = $( `<div class= ${fragmentClass} />` );
+				fragments.push( fragment );
+			}
+			return fragments; 
+		}
+		
+		// Gets the values of all CSS variables, controlling the animation
+		function getAnimationParameters( propertyHolderSelector ) {
+			return {
+				animationStartDelay: getPropertyValue( propertyHolderSelector, "--animation-start-delay" ), 
+				fragmentAnimationDuration: getPropertyValue( propertyHolderSelector, "--fragment-animation-duration" ), 
+				fragmentAnimationDelayShift: getPropertyValue( propertyHolderSelector, "--fragment-animation-delay-shift" ) 
+			};
+		}
+		
+		// Gets the value of a CSS variable 
+		function getPropertyValue( propertyHolderSelector, propertyName ) {
+			const value = parseFloat( getComputedStyle( $( propertyHolderSelector ).get( 0 ) ).getPropertyValue( propertyName ) );
+			return value;
+		}
+		
+		// Sets a CSS variable with the specified name and value
+		function setPropertyValue( propertyHolder, propertyName, propertyValue ) {
+			propertyHolder.get( 0 ).style.setProperty( propertyName, propertyValue );
+		}
 		
 	};
 }( jQuery ) );
